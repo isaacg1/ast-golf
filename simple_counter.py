4d48648a75eed8951ad514c18315a60b55fc5a7a
Isaac Grosof <isaacg@mit.edu>
2016-11-16 23:31:27 -0500

    Added rule that tokens can't be huge

--- simple_counter.py
+++ simple_counter.py
@@ -1,6 +1,15 @@
 import sys
 import ast
+import tokenize
+import io
+
 with open(sys.argv[1], 'r') as f:
     code = f.read()
 
-print(len(list(ast.walk(ast.parse(code)))))
+g = tokenize.tokenize(io.BytesIO(code.encode('utf-8')).readline)
+for token in g:
+    if len(token.string) > 15:
+        print("Illegal token:", token.string)
+        break
+else:
+    print(len(list(ast.walk(ast.parse(code)))))
