30738290807c10257d758e2287b31084638852b2
Isaac Grosof <isaacg@mit.edu>
2016-11-16 23:32:56 -0500

    Arbitray length

--- fizz_buzz4.py
+++ fizz_buzz4.py
@@ -1,5 +1,2 @@
-# 2 2    3     1  1                                          9
-for i in range(1, 101):
-#   3      2   2 2 1  2 1      2  2   2 2 1  2 1      2  2   29
+for i in range(1, int(input())):
     print((not i % 3) * "Fizz" + (not i % 5) * "Buzz" or i)  
-# 39
